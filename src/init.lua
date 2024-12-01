8b93fc0b745fc0f1773a3aa4675263587106828b
sepisoad <me@sepi.me>
2024-12-01 18:12:47 +0100

    move the code around

--- src/init.lua
+++ src/init.lua
@@ -12,6 +12,36 @@ glfw.make_context_current(window)
 -- Initialize OpenGL
 gl.init()
 
+-- Shader program
+local vertex_shader = [[
+#version 330 core
+layout(location = 0) in vec3 aPos;
+layout(location = 1) in vec3 aColor;
+
+out vec3 ourColor;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+void main()
+{
+    gl_Position = projection * view * model * vec4(aPos, 1.0);
+    ourColor = aColor;
+}
+]]
+
+local fragment_shader = [[
+#version 330 core
+in vec3 ourColor;
+out vec4 FragColor;
+
+void main()
+{
+    FragColor = vec4(ourColor, 1.0);
+}
+]]
+
 -- Cube data: positions, colors, and indices
 local positions = {
     -- Front face
@@ -78,36 +108,6 @@ gl.buffer_data("element array", gl.pack("uint", indices), "static draw")
 
 -- Unbind VAO
 gl.unbind_vertex_array()
-
--- Shader program
-local vertex_shader = [[
-#version 330 core
-layout(location = 0) in vec3 aPos;
-layout(location = 1) in vec3 aColor;
-
-out vec3 ourColor;
-
-uniform mat4 model;
-uniform mat4 view;
-uniform mat4 projection;
-
-void main()
-{
-    gl_Position = projection * view * model * vec4(aPos, 1.0);
-    ourColor = aColor;
-}
-]]
-
-local fragment_shader = [[
-#version 330 core
-in vec3 ourColor;
-out vec4 FragColor;
-
-void main()
-{
-    FragColor = vec4(ourColor, 1.0);
-}
-]]
 
 local vertex = gl.create_shader("vertex")
 gl.shader_source(vertex, vertex_shader)
