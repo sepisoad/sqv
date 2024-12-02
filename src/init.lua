ee552fbe5eaf3e9d364cadaaa64670bbf35c38ab
sepisoad <me@sepi.me>
2024-12-02 20:15:15 +0100

    wireframe cube

--- src/init.lua
+++ src/init.lua
@@ -4,21 +4,14 @@ local mth = require("moonglmath")
 
 -- Initialize GLFW
 glfw.version_hint(3, 3, "core")
-
--- Create a window
-local window = glfw.create_window(500, 400, "Spinning Cube")
+local window = glfw.create_window(800, 600, "Wireframe Cube")
 glfw.make_context_current(window)
-
--- Initialize OpenGL
 gl.init()
 
 -- Shader program
 local vertex_shader = [[
 #version 330 core
 layout(location = 0) in vec3 aPos;
-layout(location = 1) in vec3 aColor;
-
-out vec3 ourColor;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -27,85 +20,56 @@ uniform mat4 projection;
 void main()
 {
     gl_Position = projection * view * model * vec4(aPos, 1.0);
-    ourColor = aColor;
 }
 ]]
 
 local fragment_shader = [[
 #version 330 core
-in vec3 ourColor;
 out vec4 FragColor;
 
 void main()
 {
-    FragColor = vec4(ourColor, 1.0);
+    FragColor = vec4(1.0, 1.0, 1.0, 1.0); // White color
 }
 ]]
 
--- Cube data: positions, colors, and indices
+-- Cube data
 local positions = {
-    -- Front face
-    -0.1, -0.1,  0.1,
-     0.1, -0.1,  0.1,
-     0.1,  0.1,  0.1,
-    -0.1,  0.1,  0.1,
-    -- Back face
-    -0.1, -0.1, -0.1,
-     0.1, -0.1, -0.1,
-     0.1,  0.1, -0.1,
-    -0.1,  0.1, -0.1,
-}
-
-local colors = {
-    -- Front face colors
-    1.0, 0.0, 0.0,
-    0.0, 1.0, 0.0,
-    0.0, 0.0, 1.0,
-    1.0, 1.0, 0.0,
-    -- Back face colors
-    0.0, 1.0, 1.0,
-    1.0, 0.0, 1.0,
-    1.0, 1.0, 1.0,
-    0.0, 0.0, 0.0,
+    -- Cube vertices
+    -0.5, -0.5, -0.5,   0.5, -0.5, -0.5,   0.5,  0.5, -0.5,  -0.5,  0.5, -0.5,
+    -0.5, -0.5,  0.5,   0.5, -0.5,  0.5,   0.5,  0.5,  0.5,  -0.5,  0.5,  0.5,
 }
 
 local indices = {
-    -- Front face
-    0, 1, 2, 2, 3, 0,
-    -- Back face
-    4, 5, 6, 6, 7, 4,
-    -- Left face
-    0, 3, 7, 7, 4, 0,
-    -- Right face
-    1, 2, 6, 6, 5, 1,
-    -- Top face
-    3, 2, 6, 6, 7, 3,
-    -- Bottom face
-    0, 1, 5, 5, 4, 0,
+    -- Cube faces
+    0, 1, 2,  2, 3, 0,
+    4, 5, 6,  6, 7, 4,
+    0, 3, 7,  7, 4, 0,
+    1, 5, 6,  6, 2, 1,
+    3, 2, 6,  6, 7, 3,
+    0, 1, 5,  5, 4, 0,
 }
 
 -- Create VAO and buffers
 local vao = gl.new_vertex_array()
-local position_vbo = gl.new_buffer("array")
-local color_vbo = gl.new_buffer("array")
+local vbo = gl.new_buffer("array")
 local ebo = gl.new_buffer("element array")
 
--- Upload position data to GPU
-gl.bind_buffer("array", position_vbo)
+gl.bind_vertex_array(vao)
+
+-- Upload vertex data
+gl.bind_buffer("array", vbo)
 gl.buffer_data("array", gl.pack("float", positions), "static draw")
 gl.vertex_attrib_pointer(0, 3, "float", false, 0, 0)
 gl.enable_vertex_attrib_array(0)
 
--- Upload color data to GPU
-gl.bind_buffer("array", color_vbo)
-gl.buffer_data("array", gl.pack("float", colors), "static draw")
-gl.vertex_attrib_pointer(1, 3, "float", false, 0, 0)
-gl.enable_vertex_attrib_array(1)
-
--- Upload indices to GPU
+-- Upload index data
 gl.bind_buffer("element array", ebo)
 gl.buffer_data("element array", gl.pack("uint", indices), "static draw")
 
+gl.unbind_vertex_array()
+
+-- Compile shaders
 local vertex = gl.create_shader("vertex")
 gl.shader_source(vertex, vertex_shader)
 gl.compile_shader(vertex)
@@ -119,42 +83,37 @@ gl.attach_shader(prog, vertex)
 gl.attach_shader(prog, fragment)
 gl.link_program(prog)
 
--- Configure OpenGL
-gl.enable("depth test")
+-- Enable wireframe mode
+gl.polygon_mode("front and back", "line")
 
--- Transformation matrices
+-- Main rendering loop
 local projection = mth.perspective(math.rad(45.0), 800 / 600, 0.1, 100.0)
 
-gl.bind_vertex_array(vao)
-
--- Main loop
 while not glfw.window_should_close(window) do
     glfw.poll_events()
 
-    -- Clear the screen
     gl.clear_color(0.0, 0.0, 0.0, 1.0)
     gl.clear("color", "depth")
 
     -- Set transformations
     local time = glfw.get_time()
-    local model = mth.rotate(time, 0.1, 1.0, 0.1)
-    local view = mth.translate(0.0, 0.0, -1.0)
+    local model = mth.rotate(time, 0.3, 0.5, 0.7)
+    local view = mth.translate(0.0, 0.0, -2.0)
 
     gl.use_program(prog)
     gl.uniform_matrix4f(gl.get_uniform_location(prog, "model"), true, model)
     gl.uniform_matrix4f(gl.get_uniform_location(prog, "view"), true, view)
     gl.uniform_matrix4f(gl.get_uniform_location(prog, "projection"), true, projection)
 
-    -- Draw the cube
-    -- 
+    -- Draw wireframe cube
+    gl.bind_vertex_array(vao)
     gl.draw_elements("triangles", #indices, "uint", 0)
 
-    -- Swap buffers
     glfw.swap_buffers(window)
 end
 
 -- Cleanup
 gl.delete_program(prog)
 gl.delete_vertex_arrays(vao)
-gl.delete_buffers(position_vbo, color_vbo, ebo)
+gl.delete_buffers(vbo, ebo)
 glfw.terminate()
