8c1c9983be626d0d740b28ead82be1e718291f44
sepisoad <me@sepi.me>
2026-01-10 15:15:36 +0100

    linked list

--- src/deps/sepi/list.h
+++ src/deps/sepi/list.h
@@ -21,36 +21,35 @@
 /*                         TYPES                         */
 /* ===================================================== */
 
-typedef enum {
-  LIST_ERR_SUCCESS = 1,
-  LIST_INDEX_ERROR,
-  LIST_NOT_FOUND,
-  LIST_EMPTY,
-  LIST_ERR__COUNT,
-} ListError;
-
 typedef struct ListNode ListNode;
 struct ListNode {
+  RawPtr ptr;
   ListNode* next;
   ListNode* previous;
-  RawPtr data;
 };
 
-typedef struct {
-  U64 count;
+typedef struct List List;
+struct List {
+  Arena* arena;
   ListNode* head;
   ListNode* tail;
-} List;
+  U64 length;
+};
 
 /* ===================================================== */
 /*                          API                          */
 /* ===================================================== */
 
-ListError list_purge(List* l);
-ListError list_remove(List* l, U32 index);
-ListError list_push(Arena* a, List* l, RawPtr data);
-ListError list_pop(List* l, ListNode** node);
-ListError list_get(List* hm, U32 index, RawPtr* data);
+List* list_create(Arena* a);
+Nothing list_destroy(List* l);
+ListNode* list_push_tail(List* l, RawPtr ptr);
+ListNode* list_push_head(List* l, RawPtr ptr);
+ListNode* list_push_after(List* l, ListNode* n, RawPtr ptr);
+ListNode* list_push_before(List* l, ListNode* n, RawPtr ptr);
+ListNode* list_pop_tail(List* l);
+ListNode* list_pop_head(List* l);
+ListNode* list_pop(List* l, ListNode* n);
+RawPtr list_get_at(List* l, U64 index);
 
 /* ===================================================== */
 /*                    IMPLEMENTATION                     */
@@ -60,138 +59,219 @@ ListError list_get(List* hm, U32 index, RawPtr* data);
 
 extern TracyCZoneCtx trcyctx;
 
-ListError
-list_purge(List* l) {
-  TracyCZoneN(trcyctx, "list_purge", 1);
+List* list_create(Arena* a) {
+  TracyCZoneN(tracyctx, "list_create", 1);
 
-  Assert(l != 0);
-
-  // yes we leak memory for the life time of this arena!
-  // it's fine, cuz this list is not supposed to grow and
-  // shrink that much often, if the need/use case emerges
-  // then i think about it, i am not interested in generic
-  // nonsense solutions!
+  List* l = arena_push(a, sizeof(List), AlignOf(List), FALSE);
+  l->arena = a;
   l->head = 0;
   l->tail = 0;
-  l->count = 0;
+  l->length = 0;
 
-  TracyCZoneEnd(trcyctx);
-  return LIST_ERR_SUCCESS;
+  TracyCZoneEnd(tracyctx);
+  return l;
 }
 
-ListError
-list_remove(List* l, U32 index) {
-  TracyCZoneN(trcyctx, "list_remove", 1);
+Nothing list_destroy(List* l) {
+  TracyCZoneN(tracyctx, "list_destroy", 1);
 
-  Assert(l != 0);
-
-  if (index >= l->count) {
-    TracyCZoneEnd(trcyctx);
-    return LIST_INDEX_ERROR;
+  for(; l->length > 0; ) {
+    list_pop_tail(l);
   }
 
-  U32 step = 0;
-  for (ListNode* iter = l->head; iter != 0; iter = iter->next) {
-    if (step == index) {
-      iter->previous->next = iter->next;
-      iter->next->previous = iter->previous;
-      l->count--;
-
-      TracyCZoneEnd(trcyctx);
-      return LIST_ERR_SUCCESS;
-    }
-    step++;
-  }
-
-  TracyCZoneEnd(trcyctx);
-  return LIST_NOT_FOUND;
+  TracyCZoneEnd(tracyctx);
 }
 
-ListError
-list_push(Arena* a, List* l, RawPtr data) {
-  TracyCZoneN(trcyctx, "list_push", 1);
+ListNode* list_push_tail(List* l, RawPtr ptr) {
+  TracyCZoneN(tracyctx, "list_push_tail", 1);
 
-  Assert(a != 0);
-  Assert(l != 0);
-  Assert(data != 0);
+  ListNode* n = arena_push(l->arena, sizeof(ListNode), AlignOf(ListNode), TRUE);
+  n->ptr = ptr;
+  l->length++;
 
-  ListNode* node =
-      (ListNode*)arena_push(a, sizeof(ListNode), AlignOf(ListNode), TRUE);
-
-  node->data = data;
-  if (l->count == 0) {
-    l->head = l->tail = node;
+  if(!l->tail) {
+    l->tail = n;
+    l->head = n;
   } else {
-    node->previous = l->tail;
-    l->tail->next = node;
-    l->tail = node;
+    n->previous = l->tail;
+    l->tail->next = n;
+    l->tail = n;
   }
 
-  l->count++;
+  TracyCZoneEnd(tracyctx);
 
-  TracyCZoneEnd(trcyctx);
-  return LIST_ERR_SUCCESS;
+  return n;
 }
 
-ListError
-list_pop(List* l, ListNode** node) {
-  TracyCZoneN(trcyctx, "list_pop", 1);
+ListNode* list_push_head(List* l, RawPtr ptr) {
+  TracyCZoneN(tracyctx, "list_push_head", 1);
 
-  Assert(l != 0);
-  Assert(node != 0);
+  ListNode* n = arena_push(l->arena, sizeof(ListNode), AlignOf(ListNode), TRUE);
+  n->ptr = ptr;
+  l->length++;
 
-  ListError err = LIST_ERR_SUCCESS;
+  if(!l->head) {
+    l->tail = n;
+    l->head = n;
+  } else {
+    n->next = l->head;
+    l->head->previous = n;
+    l->head = n;
+  }
 
-  if (l->count == 0) {
-    err = LIST_EMPTY;
-    *node = 0;
+  TracyCZoneEnd(tracyctx);
+  return n;
+}
+
+ListNode* list_push_after(List* l, ListNode* n, RawPtr ptr) {
+  TracyCZoneN(tracyctx, "list_push_after", 1);
+
+  ListNode* nn = arena_push(l->arena, sizeof(ListNode), AlignOf(ListNode), TRUE);
+  nn->ptr = ptr;
+  if (n->next) {
+    n->next->previous = nn;
+  }
+  nn->next = n->next;
+  nn->previous = n;
+  n->next = nn;
+
+  l->length++;
+
+  TracyCZoneEnd(tracyctx);
+
+  return nn;
+}
+
+ListNode* list_push_before(List* l, ListNode* n, RawPtr ptr) {
+  TracyCZoneN(tracyctx, "list_push_before", 1);
+
+  ListNode* nn = arena_push(l->arena, sizeof(ListNode), AlignOf(ListNode), TRUE);
+  nn->ptr = ptr;
+  nn->next = n;
+  if (n->previous) {
+    nn->previous = n->previous;
+    n->previous->next = nn;
+  } else {
+    l->head = nn;
+  }
+
+  n->previous = nn;
+
+  l->length++;
+
+  TracyCZoneEnd(tracyctx);
+
+  return nn;
+}
+
+ListNode* list_pop_tail(List* l) {
+  TracyCZoneN(tracyctx, "list_pop_tail", 1);
+
+  ListNode* res = 0;
+
+  if(!l->tail) {
     goto cleanup;
   }
 
-  if (l->count == 1) {
-    *node = l->tail;
-    l->head = l->tail = 0;
-  } else {
-    *node = l->tail;
-    l->tail = l->tail->previous;
-    l->tail->next = 0;
+  l->length--;
+  res = l->tail;
+
+  if(!l->tail->previous) {
+    goto cleanup;
   }
 
-  l->count--;
+  l->tail = l->tail->previous;
+  l->tail->next = 0;
+
 
 cleanup:
-  TracyCZoneEnd(trcyctx);
-  return err;
+  TracyCZoneEnd(tracyctx);
+
+  return res;
 }
 
-ListError
-list_get(List* l, U32 index, RawPtr* data) {
-  TracyCZoneN(trcyctx, "list_get", 1);
+ListNode* list_pop_head(List* l) {
+  TracyCZoneN(tracyctx, "list_pop_head", 1);
 
-  Assert(l != 0);
-  Assert(data != 0);
+  ListNode* res = 0;
 
-  if (index >= l->count) {
-    TracyCZoneEnd(trcyctx);
-    return LIST_INDEX_ERROR;
+  if(!l->head) {
+    goto cleanup;
   }
 
-  U32 step = 0;
-  for (ListNode* iter = l->head; iter != 0; iter = iter->next) {
-    if (step == index) {
-      iter->previous->next = iter->next;
-      iter->next->previous = iter->previous;
-      *data = iter->data;
+  l->length--;
+  res = l->head;
 
-      TracyCZoneEnd(trcyctx);
-      return LIST_ERR_SUCCESS;
-    }
-    step++;
+  if(!l->head->next) {
+    goto cleanup;
   }
 
-  TracyCZoneEnd(trcyctx);
-  return LIST_NOT_FOUND;
+  l->head = l->head->next;
+  l->head->previous = 0;
+
+
+cleanup:
+  TracyCZoneEnd(tracyctx);
+
+  return res;
 }
+
+ListNode* list_pop(List* l, ListNode* n) {
+  TracyCZoneN(tracyctx, "list_pop", 1);
+
+  ListNode* res = 0;
+
+  if (!l->head || !n) {
+    goto cleanup;
+  }
+
+  if (n == l->tail) {
+    res = list_pop_tail(l);
+    goto cleanup;
+  }
+
+  if (n == l->head) {
+    res = list_pop_head(l);
+    goto cleanup;
+  }
+
+  l->length--;
+  n->previous->next = n->next;
+  n->next->previous = n->previous;
+  n->next = 0;
+  n->previous = 0;
+
+cleanup:
+  TracyCZoneEnd(tracyctx);
+
+  return res;
+}
+
+RawPtr list_get_at(List* l, U64 index) {
+  TracyCZoneN(tracyctx, "list_get_at", 1);
+
+  ListNode* res = 0;
+
+  if (!l->head) {
+    goto cleanup;
+  }
+
+  if (index >= l->length) {
+    goto cleanup;
+  }
+
+  res = l->head;
+  U64 pos = 0;
+
+  for( ; pos != index ; res = res->next, pos++);
+
+cleanup:
+  TracyCZoneEnd(tracyctx);
+
+  return res->ptr;
+}
+
+
 
 /* ===================================================== */
 /*                          END                          */
